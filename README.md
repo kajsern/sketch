d85dd73d416b92953a1ae429bc95d8944638f7d5
Stefan Pärson <stefanparson@gmail.com>
2016-12-17 19:12:17 +0100

    Finished project

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 This will be my first project with jQuery, and it will be exciting!
+
+Afterthought.
+I got stuck a few times but always managed to find a solution right before i wanted to end my life, as i usually do. I am not sure if this is because of better coding skills, better google skills, or better luck. But there it is. Obviously this could be prettier but Im trying not to be a perfectionist and work through the material.
--- myScript.js
+++ myScript.js
@@ -1,15 +1,51 @@
-$(document).ready(function(){
-    var html="<tr>";
-    for (i=0; i<16; i++){
-	for(j=0;j<16; j++){
-	    html+="<td class='gridelement'></td>";
+$(document).ready(function main(){
+    function mktable(size){
+	if(size==undefined){
+	    var html="<tr>";
+	    for (i=0; i<16; i++){
+		for(j=0;j<16; j++){
+		    html+="<td class='gridelement'></td>";
+		}
+		html+="</tr><tr>";
+	    }
+	    html+="</tr>";
+	    $("table").append(html);
+	    
+	    $(".gridelement").mouseenter(function(){
+		$(this).css("background", "black");
+	    });
+	    var userInput;
+	    $("button").click(function(){
+		$(".gridelement").css("background","white");
+		userInput = prompt("Size of grid?");
+		$("table").html("");
+		mktable(userInput);
+	    });
 	}
-	html+="</tr><tr>";
+	
+	else{
+	    var html="<tr>";
+	    for (i=0; i<size; i++){
+		for(j=0;j<size; j++){
+		    html+="<td class='gridelement'></td>";
+		}
+		html+="</tr><tr>";
+	    }
+	    html+="</tr>";
+	    $("table").append(html);
+	    
+	    $(".gridelement").mouseenter(function(){
+		$(this).css("background", "black");
+	    });
+	    var userInput;
+	    $("button").click(function(){
+		$(".gridelement").css("background","white");
+		userInput = prompt("Size of grid?");
+		$("table").html("");
+		mktable(userInput);
+	    });
+	}
     }
-    html+="</tr>";
-    $(".container").append(html);
+    mktable();
     
-    $(".gridelement").mouseenter(function(){
-	$(this).css("background", "black");
-    });    
 });
